17fb72792e91d05a0ebe6dd6178cd49759f46161
Peixe <thefish0987@hotmail.com>
2018-08-02 13:59:43 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -23,19 +23,31 @@ class App extends Component {
       gols2: 0
     });
   }
+
+  //Toda função de eventoq que voce cria é um callback. O proprio sistema express carrega o (req,res), ou até o err, mas especificamente para esse funçãp voce que tem que criar , pois voce declarou evento vc espera receber . Recebe o evento que ja seria passado para a funçao e recebe o target dele, então o event.targer é o THIS.
+
+  //Não deu certo do modo abaixo, do modo que o professor fez no github está ok. Tentar corrigir.
+
+  renomear = (evento) => {
+    //2 - Nesse setState toda vez que ele é chamaodo atualiza o nome.
+    if (evento.target.name="1"){
+      this.setState({time1: evento.target.value})}
+    else if (evento.target.name="2"){
+      this.setState({time2: evento.target.value})}
+}
   
   render() {
     return (
       <div>
         <div className="controles">
-          <input type="text" placeholder="Digite o nome do time da casa" />
-          <input type="text" placeholder="Digite o nome do time visitante" />
+          <input onChange={this.renomear} type="text" placeholder="Digite o nome do time da casa" value={this.state.time1} name="1" />
+          <input onChange={this.renomear} type="text" placeholder="Digite o nome do time visitante" value={this.state.time2} name="2"/>
           <button onClick={this.atualizar}>Zerar placar</button>
         </div>
         <div className="placar">
-          <PlacarTime numero="1" gols={this.state.gols1} nomeTime="Corinthians" funcaoBotao={this.incrementar} />
+          <PlacarTime numero="1" gols={this.state.gols1} nomeTime={this.state.time1} funcaoBotao={this.incrementar} />
           X
-          <PlacarTime numero="2" gols={this.state.gols2}nomeTime="Chapecoense" funcaoBotao={this.incrementar} visitante="true"/>
+          <PlacarTime numero="2" gols={this.state.gols2} nomeTime={this.state.time2} funcaoBotao={this.incrementar} visitante="true"/>
         </div>
       </div>
     );
